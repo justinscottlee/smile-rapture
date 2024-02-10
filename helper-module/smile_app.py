4d7f4dc81c750601b67ef61a9b86deab521b0d8e
justinscottlee <justin.s.lee.work@gmail.com>
2024-02-10 14:00:29 -0800

    defined node and container structure within experiment object

--- helper-module/smile_app.py
+++ helper-module/smile_app.py
@@ -1,4 +1,4 @@
-EXPERIMENT_ID = "8107013c8f744b308740d4c60cd83e2a"
+EXPERIMENT_UUID = "8107013c8f744b308740d4c60cd83e2a"
 
 import zmq
 import requests
@@ -8,7 +8,7 @@ socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
 def api_upload(json_data: dict):
-    requests.post(f"http://localhost:5001/api/upload/{EXPERIMENT_ID}", json=json_data)
+    requests.post(f"http://localhost:5001/api/upload/{EXPERIMENT_UUID}", json=json_data)
 
 while True:
     message = socket.recv_json()
@@ -17,4 +17,4 @@ while True:
              api_upload(message)
              socket.send_json({})
         case "GET_EXPERIMENT_ID":
-            socket.send_json({"experiment_id": f"{EXPERIMENT_ID}"})
+            socket.send_json({"experiment_id": f"{EXPERIMENT_UUID}"})
--- web-portal/models.py
+++ web-portal/models.py
@@ -13,6 +13,16 @@ class ExperimentStatus(Enum):
     COMPLETED = 4
     RESULTS_READY = 5
 
+class ContainerStatus(Enum):
+    PENDING = 0 # Kubernetes is trying to download the container image from the registry
+    RUNNING = 1
+    SUCCEEDED = 2
+    FAILED = 3
+
+class NodeType(Enum):
+    DRONE_ARM64 = 0
+    NODE_ARM64 = 1
+    NODE_AMD64 = 2
 
 @dataclass
 class ResultEntry:
@@ -28,13 +38,24 @@ class User:
     experiment_ids: list[UUID.hex] = field(default_factory=list)
     admin: bool = False
 
+@dataclass
+class Container:
+    src_dir: str # user-uploaded source code directory
+    python_requirements: str # path to python requirements.txt
+    registry_tag: str # unique name for this container image
+    ports: list[int] # list of ports to open within kubernetes network for inter-container communication
+    status: ContainerStatus
+
+@dataclass
+class Node:
+    type: NodeType
+    containers: list[Container]
 
 @dataclass
 class Experiment:
-    experiment_name: str = "experiment"
+    experiment_uuid: str
     status: ExperimentStatus = ExperimentStatus.NOT_READY
-    registry_tag: str = ""
-    port_map: list[int] = field(default_factory=list)
     results: list[ResultEntry] = field(default_factory=list)
     created_at: float = field(default_factory=time.time)
     created_by: str = "none"
+    nodes: list[Node]
