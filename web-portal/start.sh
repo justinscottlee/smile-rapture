5c825a3645194b184872ad9d20200d6de5ec209c
justinscottlee <justin.s.lee.work@gmail.com>
2024-02-11 14:28:27 -0800

    added start script to more quickly test changes

--- web-portal/start.sh
+++ web-portal/start.sh
@@ -1,1 +1,1 @@
-docker buildx build --push --platform linux/arm64,linux/amd64 --tag $1/registry/$2 . --output=type=registry,registry.insecure=true
+sudo bash -c ". /home/e302d/pyenv/bin/activate; python app.py"
