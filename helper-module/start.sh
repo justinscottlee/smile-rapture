bd275a9c868c934176843c4fe7b973d0e1609e62
justinscottlee <justin.s.lee.work@gmail.com>
2024-02-19 12:08:46 -0800

    Fixed errors with invalid characters on json post

--- helper-module/start.sh
+++ helper-module/start.sh
@@ -5,8 +5,9 @@ container_id=""
 
 send_stdout() {
   local line=$1
-  server_url="http://130.191.161.13:5001/api/upload/$experiment_id/$container_id/stdout"
-  curl -X POST -H "Content-Type: application/json" -d "{"stdout": "$line"}" "$server_url"
+  local json_payload=$(jq -nc --arg line "$line" '{"stdout": $line}')
+  local server_url="http://130.191.161.13:5001/api/upload/$experiment_id/$container_id/stdout"
+  curl -X POST -H "Content-Type: application/json" -d "$json_payload" "$server_url"
 }
 
 # Run the Python script and process its stdout
