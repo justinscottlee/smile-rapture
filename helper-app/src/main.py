d323b500fb7af9b3d4a2a69aee5219ff57256214
justinscottlee <justin.s.lee.work@gmail.com>
2024-03-10 13:24:55 -0700

    Added functionality to control robot through rapture

--- helper-app/src/main.py
+++ helper-app/src/main.py
@@ -1,4 +1,5 @@
 EXPERIMENT_UUID = "72334461b203499386f832eed9a9f64d"
+ROBOT_NAME = "rpi4"
 
 import zmq
 import requests
@@ -6,6 +7,9 @@ import requests
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
+
+robot_socket = context.socket(zmq.REQ)
+robot_socket.connect(f"tcp://{ROBOT_NAME}:5555")
 
 def api_upload(json_data: dict):
     requests.post(f"http://130.191.161.13:5001/api/upload/{EXPERIMENT_UUID}", json=json_data)
@@ -18,3 +22,7 @@ while True:
              socket.send_json({})
         case "GET_EXPERIMENT_ID":
             socket.send_json({"experiment_id": f"{EXPERIMENT_UUID}"})
+        case "MOVE":
+            robot_socket.send_json(message)
+            response = robot_socket.recv_json()
+            socket.send_json(response)
--- helper-module/smile.py
+++ helper-module/smile.py
@@ -23,7 +23,8 @@ def log(message: str, level="INFO", **data):
     }
     request.update(data)
     socket.send_json(request)
-    socket.recv_string()
+    response = socket.recv_json()
+    return response["status"]
 
 """Retrieve the experiment ID."""
 def get_experiment_id():
@@ -35,3 +36,55 @@ def get_experiment_id():
     socket.send_json(request)
     response = socket.recv_json()
     return response["experiment_id"]
+
+def robot_moveforward(move_speed, move_time):
+    if debug:
+        return
+    request = {
+        "type": "MOVE",
+        "direction": "FORWARD",
+        "move_speed": move_speed,
+        "move_time": move_time
+    }
+    socket.send_json(request)
+    response = socket.recv_json()
+    return response["status"]
+
+def robot_movebackward(move_speed, move_time):
+    if debug:
+        return
+    request = {
+        "type": "MOVE",
+        "direction": "BACKWARD",
+        "move_speed": move_speed,
+        "move_time": move_time
+    }
+    socket.send_json(request)
+    response = socket.recv_json()
+    return response["status"]
+
+def robot_moveright(move_speed, move_time):
+    if debug:
+        return
+    request = {
+        "type": "MOVE",
+        "direction": "RIGHT",
+        "move_speed": move_speed,
+        "move_time": move_time
+    }
+    socket.send_json(request)
+    response = socket.recv_json()
+    return response["status"]
+
+def robot_moveleft(move_speed, move_time):
+    if debug:
+        return
+    request = {
+        "type": "MOVE",
+        "direction": "LEFT",
+        "move_speed": move_speed,
+        "move_time": move_time
+    }
+    socket.send_json(request)
+    response = socket.recv_json()
+    return response["status"]
