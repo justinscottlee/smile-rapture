7e374dccab1ffd52d275df73c1209d7a80afeb77
justinscottlee <justin.s.lee.work@gmail.com>
2024-03-10 13:34:45 -0700

    added test app for moving the robot.

---
+++ test-apps/robot-moving/src/main.py
@@ -0,0 +1,11 @@
+import smile
+
+print("Move forward")
+smile.robot_moveforward(50, 1)
+print("Move backward")
+smile.robot_movebackward(50, 2)
+print("Move left")
+smile.robot_moveleft(50, 1)
+print("Move right")
+smile.robot_moveright(50, 1)
+print("done!")
