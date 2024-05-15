aa3586dcd472b58232723a31ea4bd98f63d4757c
Nick Palmer <npalmer2267@sdsu.edu>
2024-05-15 10:23:51 -0700

    add function to check docker registry state

--- rapture_website/app/services/kube.py
+++ rapture_website/app/services/kube.py
@@ -1,4 +1,5 @@
 import kubernetes
+import requests
 
 try:
     kubernetes.config.load_kube_config(config_file="/etc/rancher/k3s/k3s.yaml")
@@ -17,3 +18,19 @@ def get_kube_connected_state():
     except Exception as e:
         print(f"Failed to connect to Kubernetes API: {e}")
         return "API connection failed"
+
+
+def check_docker_registry_status(registry_url):
+    try:
+        # Typically, the /v2/_catalog endpoint can be used to list repositories, if accessible.
+        response = requests.get(f"{registry_url}/v2/_catalog")
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            return True
+        else:
+            print(f"Failed to reach Docker registry. Status code: {response.status_code}")
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to Docker registry: {e}")
+        return False
