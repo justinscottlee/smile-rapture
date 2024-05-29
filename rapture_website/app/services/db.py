e44911b3089051ee295ef538b65a87e6585ada49
Nick Palmer <npalmer2267@sdsu.edu>
2024-05-29 12:42:09 -0700

    Add db delete functions

--- rapture_website/app/services/db.py
+++ rapture_website/app/services/db.py
@@ -7,6 +7,18 @@ config_collection = db["config"]
 user_collection = db["users"]
 experiment_collection = db["experiments"]
 
+
+def delete_all_users():
+    user_collection.delete_many({})
+
+
+def delete_all_experiments():
+    experiment_collection.delete_many({})
+
+
+def delete_all_configs():
+    config_collection.delete_many({})
+
 # To delete all data: for testing!
 # config_collection.delete_many({})
 # user_collection.delete_many({})
